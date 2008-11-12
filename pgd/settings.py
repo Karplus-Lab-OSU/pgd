a2f4b9fed00ffeaab4ec22d8131990a8cf5fb265
peter <peter@doors.(none)>
2008-11-12 09:24:11 -0800

    added my local settings

--- pgd/settings.py
+++ pgd/settings.py
@@ -9,10 +9,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'pgd'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'pgd'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'pgd'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -77,4 +77,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
+    'pgd.tasks',
+    'pgd.taskstest',
 )
