833afe266732e9139810c5d4c4f5e0b084e60e75
peter <peter@doors.(none)>
2008-12-03 09:50:27 -0800

    url for search form was preventing plot urls from functioning

--- pgd/pgd_search/urls.py
+++ pgd/pgd_search/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('pgd_search.views',
-    (r'^', 'search'),
+    (r'^$', 'search'),
     (r'^plot/svg/$', 'renderToSVG'),
     (r'^plot/png/$', 'renderToPNG'),
 )
