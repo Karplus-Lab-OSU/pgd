564bf9458a06a0695f33db2ef03c78afd6e44e7d
davidsjo <davidsjo@onid.orst.edu>
2008-12-04 15:17:20 -0800

    tiny change to validation regex

--- pgd/pgd_search/SearchParser.py
+++ pgd/pgd_search/SearchParser.py
@@ -4,7 +4,7 @@ import re
 Validates a field to make sure that it has valid syntax for a search field
 """
 def validateQueryField(str):
-    r = re.compile(r'^(-?([1-9]\d*|0)(\.\d+)?|(\.\d+))-(-?([1-9]\d*|0)(\.\d+)?|(\.\d+))(,(-?([1-9]\d*|0)(\.\d+)?|(\.\d+))-(-?([1-9]\d*|0)(\.\d+)?|(\.\d+)))*$')
+    r = re.compile(r'^(-?([1-9]\d*|0)(\.\d+)?|(\.\d+))(-(-?([1-9]\d*|0)(\.\d+)?|(\.\d+)))?(,(-?([1-9]\d*|0)(\.\d+)?|(\.\d+))(-(-?([1-9]\d*|0)(\.\d+)?|(\.\d+)))?)*$')
     m = r.match(str)
     return not m == None
 
