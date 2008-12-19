affb756b858a43a3a57665ccd6dc2f6fe9b44b56
Peter Krenesky <peter@hendrix.(none)>
2008-12-19 11:53:26 -0800

    adding search results browse page

---
+++ pgd/pgd_search/browse/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+import math
+
+"""
+display search results in tabular form
+"""
+def browse(request):
+
+    # get search from session
+    search = request.session['search']
+    segments = search.querySet()
+    paginator = Paginator(segments, 5) # Show 10 segments per page
+
+    # Make sure page request is an int. If not, deliver first page.
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    # If page request (9999) is out of range, deliver last page of results.
+    try:
+        paginatedSegments = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paginatedSegments = paginator.page(paginator.num_pages)
+
+    #generate iValues
+    iIndex = int(math.ceil(search.residueCount/2.0)-1)
+    start = 0 - (search.residueCount-1) / 2
+    stop  = int(math.ceil((search.residueCount-1) / 2.0))+1
+    iValues = [
+            # Generates a series of tuples (<value>,<signed string of value>); zero is 'i'
+            '%+i'%i if i else 'i' for i in range(start,stop)
+    ]
+
+    return render_to_response('browse.html', {
+        'segments': paginatedSegments,
+        'iValues' : iValues,
+        'segmentSlice':'%i:%i'%(iIndex+start,iIndex+stop)
+    }, context_instance=RequestContext(request))
--- pgd/pgd_search/urls.py
+++ pgd/pgd_search/urls.py
@@ -4,6 +4,7 @@ from pgd_search.search.views import search
 from pgd_search.plot.views import renderToSVG, renderToPNG, plotDump
 from pgd_search.statistics.views import searchStatistics
 from pgd_search.dump.views import dataDump
+from pgd_search.browse.views import browse
 
 urlpatterns = patterns('',
     (r'^$', search),
@@ -13,4 +14,5 @@ urlpatterns = patterns('',
     (r'^plot/dump/$', plotDump),
     (r'^statistics/$', searchStatistics),
     (r'^dump/$', dataDump),
+    (r'^browse/$', browse),
 )
