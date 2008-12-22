50320cd9d54eaa5944f4a3a4d0b553612ebecab2
peter <peter@doors.(none)>
2008-12-22 13:04:18 -0800

    Speed optimization for Statistics page 	* fairly significant refactor to combine loops 	* only needed fields are queried now 	* residues are queried in groups, by AA         * reduced execution time by ~64%

--- pgd/pgd_search/statistics/views.py
+++ pgd/pgd_search/statistics/views.py
@@ -4,25 +4,36 @@ from django.conf import settings
 from statlib import stats
 
 from constants import AA_CHOICES
-from pgd_search.models import Search, Segment
-
+from pgd_search.models import Search, Segment, iIndex
 
 """
 display statistics about the search
 """
 def searchStatistics(request):
 
-    stat_attributes = ['L1','L2','L3','L4','L5','a1','a2','a3','a4','a5','a6','a7']
+    stat_attributes_base = ['L1','L2','L3','L4','L5','a1','a2','a3','a4','a5','a6','a7']
     TOTAL_INDEX = {'na':0,'e':1,'E':2,'S':3,'h':4,'H':5,'t':6,'T':7,'g':8,'G':9,'B':10,'i':11,'I':12}
-    STAT_INDEX = {'L1':0,'L2':1,'L3':2,'L4':3,'L5':4,'a1':5,'a2':6,'a3':7,'a4':8,'a5':9,'a6':10,'a7':11}
+    STAT_INDEX = {}
+
+    ss_field = 'r%i_ss' % iIndex
+    aa_field = 'r%i_aa' % iIndex
+
+    stat_attributes = ['r%i_%s'%(iIndex, f) for f in stat_attributes_base]
+    fieldNames      = ['r%i_%s'%(iIndex, f) for f in stat_attributes_base]
+    fieldNames.append(ss_field)
+
+    for i in range(len(stat_attributes)):
+        STAT_INDEX[stat_attributes[i]] = i
 
     # get search from session
     search = request.session['search']
     searchQuery = search.querySet()
 
     peptides = {}
+    #iterate through the aa_choices
     for code,long_code in AA_CHOICES:
-        peptides[code] = {
+        #create data structure
+        peptide = {
                 'longCode':long_code,
                 #total
                 'total':0,
@@ -32,30 +43,24 @@ def searchStatistics(request):
                 'stats':[['L1',[]],['L2',[]],['L3',[]],['L4',[]],['L5',[]],['a1',[]],['a2',[]],['a3',[]],['a4',[]],['a5',[]],['a6',[]],['a7',[]]]
             }
 
-    #iterate through all the segments
-    for segment in searchQuery:
+        #query segments matching this AA with just the fields we want to perform calcuations on
+        residueData = searchQuery.filter(**{aa_field:code}).values(*fieldNames)
+        peptide['total'] = searchQuery.filter(**{aa_field:code}).count()
 
-        #get the i protein
-        segment = segment.residues[4]
+        #iterate through all the segment data
+        for data in residueData:
 
-        #get the peptide
-        peptide = peptides[segment.aa]
+            #calculate values
+            if data[ss_field] == ' ':
+                peptide['counts'][TOTAL_INDEX['na']][1] += 1
+            else:
+                peptide['counts'][TOTAL_INDEX[data[ss_field]]][1] += 1
 
-        #calculate values
-        peptide['total'] += 1
-        if segment.ss == ' ':
-            peptide['counts'][TOTAL_INDEX['na']][1] += 1
-        else:
-            peptide['counts'][TOTAL_INDEX[segment.ss]][1] += 1
+            #store all values for attributes into arrays
+            for key in stat_attributes:
+                peptide['stats'][STAT_INDEX[key]][1].append(data[key])
 
-        #store all values for attributes into arrays
-        for key in stat_attributes:
-            peptide['stats'][STAT_INDEX[key]][1].append(segment.__dict__[key])
-
-
-    # iterate through all the datastructures calculating statistics
-    for key in peptides:
-        peptide = peptides[key]
+        #calculate statistics
         for attribute in stat_attributes:
             list = peptide['stats'][STAT_INDEX[attribute]][1]
             list_len = len(list)
@@ -81,8 +86,9 @@ def searchStatistics(request):
 
             peptide['stats'][STAT_INDEX[attribute]][1] = {'mean':mean,'std':stdev,'min':range_min, 'max':range_max}
 
+        peptides[code] = peptide
 
     return render_to_response('stats.html', {
-        'attributes': stat_attributes,
+        'attributes': stat_attributes_base,
         'peptides':peptides
     }, context_instance=RequestContext(request))
