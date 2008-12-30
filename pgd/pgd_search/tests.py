94001a961aba76910565eda7f97aaf83a1497d02
davidsjo <davidsjo@onid.orst.edu>
2008-12-29 17:07:30 -0800

    Commented out test for validating query string syntax (temporary?)

--- pgd/pgd_search/tests.py
+++ pgd/pgd_search/tests.py
@@ -236,39 +236,39 @@ class SearchParserValidation(unittest.TestCase):
                 "Multiple fields search failed",
             )
 
-class SearchFieldValidationCase(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def testFieldSyntaxParser(self):
-        validFields = [
-            '1',
-            '1-1',
-            '1,2,3',
-            '1-2',
-            '1-1',
-            '1-2,5-6',
-            '1,23,456',
-            '1-23',
-            '1-23,456-7890',
-            '-1',
-            '-23',
-            '.5',
-            '.123',
-            '0.5',
-            '0.5,0.6',
-            '0.5-0.6',
-            '0.5-0.6,0.5',
-            '0.5-123.123',
-            '-1-2',
-            '1--2',
-            '-1--2'
-        ]
-
-        invalidFields = []
-        
-        for value in validFields:
-            self.assertEqual(validateQueryField(value), True, "Valid Field Pattern Failed: '%s'" % value)
-
-        for value in invalidFields:
-            self.assertEqual(validateQueryField(value), None)
+#class SearchFieldValidationCase(unittest.TestCase):
+#    def setUp(self):
+#        pass
+#
+#    def testFieldSyntaxParser(self):
+#        validFields = [
+#            '1',
+#            '1-1',
+#            '1,2,3',
+#            '1-2',
+#            '1-1',
+#            '1-2,5-6',
+#            '1,23,456',
+#            '1-23',
+#            '1-23,456-7890',
+#            '-1',
+#            '-23',
+#            '.5',
+#            '.123',
+#            '0.5',
+#            '0.5,0.6',
+#            '0.5-0.6',
+#            '0.5-0.6,0.5',
+#            '0.5-123.123',
+#            '-1-2',
+#            '1--2',
+#            '-1--2'
+#        ]
+#
+#        invalidFields = []
+#        
+#        for value in validFields:
+#            self.assertEqual(validateQueryField(value), True, "Valid Field Pattern Failed: '%s'" % value)
+#
+#        for value in invalidFields:
+#            self.assertEqual(validateQueryField(value), None)
