31f72f38a6ae48ebe5d6769adc3a737f52c2278a
Peter Krenesky <peter@osuosl.org>
2009-07-08 13:32:29 -0700

    removing old tasks code which is now obsolete

--- pgd/tasks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('pgd.tasks.views',
-    (r'^$', 'showtasks'),
-    (r'^progress/$', 'taskprogress'),
-    (r'^start/$', 'starttask'),
-)
