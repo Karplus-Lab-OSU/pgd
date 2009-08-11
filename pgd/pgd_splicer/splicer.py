ca6510f1fa14094014e4e3bbd4ead2c5678145c7
Peter Krenesky <peter@osuosl.org>
2009-08-11 14:53:51 -0700

    Splicer update:    * fix for selector. threshold 90 was overriting threshold 25    * version is now set on import

--- pgd/pgd_splicer/splicer.py
+++ pgd/pgd_splicer/splicer.py
@@ -30,6 +30,7 @@ class SplicerInput(forms.Form):
     thresholds = forms.CharField(initial='25,90', help_text='Enter a comma delimeted list of thresholds to include')
     resolution = forms.FloatField(label='Max Resolution', initial='3.0', help_text='Enter the max resolution to accept')
     rfactor    = forms.FloatField(label='Max R-Factor', initial='1.0', help_text='Enter the max r-factor to accept')
+    batch_size = forms.IntegerField(label='Batch Size', initial='50', help_text='Enter the number of proteins to include per workunit.  A protein takes between 1 and 15 seconds to process depending on chain count.  Proteins that are up to date will take only about 1 second or less to process.  Batches should ideally be a few minutes long.  A good benchmark is 25-50 for a full import.  1000 for a weekly update with only a few hundred proteins to update.')
 
 
 class SplicerTask(TaskContainer):
@@ -44,5 +45,5 @@ class SplicerTask(TaskContainer):
     def __init__(self, msg=None):
         TaskContainer.__init__(self, msg)
 
-        self.add_task(DunbrackPDBSelectorTask('Select PDBs for download'))
+        self.add_task(DunbrackPDBSelectorTask('Select PDBs for download'),1)
         self.add_task(ParallelProteinImportTask())
