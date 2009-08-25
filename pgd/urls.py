52168d4af9ea883782840d27345baa86f94d9db7
Sean Connell <sean@sean-laptop.(none)>
2009-08-25 15:54:25 -0700

    Fixed a template mistake.

--- pgd/urls.py
+++ pgd/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
-from registration.views import activate
-from registration.views import register
 from django.contrib.auth import authenticate, logout
 import settings
 from django.contrib import admin
@@ -27,14 +25,6 @@ urlpatterns = patterns('',
                          'extra_context': {'SITE_ROOT':settings.SITE_ROOT,
                                            'MEDIA_ROOT': settings.MEDIA_URL}}),
 )
-
-'''urlpatterns += patterns('',
-  (r'^accounts/profile/$', direct_to_template, {'template': 'registration/profile.html'}),
-  (r'^accounts/password_reset/$', password_reset, {'template_name': 'registration/password_reset.html'}),
-  (r'^accounts/password_reset_done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}),
-  (r'^accounts/password_change/$', password_change, {'template_name': 'registration/password_change.html'}),
-  (r'^accounts/password_change_done/$', password_change_done, {'template_name': 'registration/password_change_done.html'}),
-)'''
 
 #The following is used to serve up local media files like images
 #if settings.LOCAL_DEV:
