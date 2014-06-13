19ef145d149053d346769d5976a350d1d5b62e5a
sarvesh-onlyme <sarvesh.onlyme@gmail.com>
2014-06-14 02:37:43 +0530

    {% url %} changed in templates

--- pgd/settings.py
+++ pgd/settings.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'osuosl',                      # Or path to database file if using sqlite3.
-        'USER': 'USERNAME',                      # Not used with sqlite3.
-        'PASSWORD': 'PASSWORD',                  # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '1991',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -129,7 +129,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    './templates'
+    './templates',
 )
 
 INSTALLED_APPS = (
