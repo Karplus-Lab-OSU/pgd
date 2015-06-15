87964f88a0a4a8eb6fe9232be5b64a80688a6db5
Ramana.S <vxrram95@gmail.com>
2015-06-16 01:04:03 +0530

    Minor Changes

--- pgd/urls.py
+++ pgd/urls.py
@@ -18,8 +18,8 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    #(r'^admin/(.*)', admin.site.root),
-    (r'^admin/', include(admin.site.urls)),
+    #(r'^admin/', include(admin.site.urls)),
+    
     (r'^search/', include('pgd_search.urls')),
       
     #override the default urls
