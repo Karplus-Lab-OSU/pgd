7786dc891c98a634678055d78cfd19921da64515
Jack Twilley <mathuin@gmail.com>
2016-05-20 10:12:40 -0700

    Updated Docker Compose configuration to v2.
    Docker Compose released a new configuration version recently.  Certain features we were using in version 1 are being deprecated, such as environment variables.  Additionally, data-only containers are no longer best-practice now that data volumes are released.
    
    The configuration file now uses data volumes and networks as encouraged by the new configuration version.
    
    The change in environment variable support necessitated a change in the database configuration in our settings.  This change will break the settings.ini file on production and staging, but the fix is straightforward.
    
    Some other minor but useful and related changes to dev-setup.sh also occurred.

--- dev-setup.sh
+++ dev-setup.sh
@@ -2,22 +2,29 @@
 
 # clean up current situation
 docker-compose stop
-docker-compose rm --force
+docker-compose rm -v --force
 find . \( -name "*.pyc" -o -name "*.pyo" \) -print0 | xargs -0 rm -rf
 
 # rebuild containers
 docker-compose build
 
 # start data containers
-docker-compose up -d mysql pdb
+docker-compose up -d mysql
 
 # collectstatic
-docker-compose run web python manage.py collectstatic --noinput
+docker-compose run --rm web python manage.py collectstatic --noinput
 
 # create database
 # due to race conditions, this may require multiple attempts
 retcode=1
-while [ $retcode -ne 0 ]; do
-    docker-compose run web python manage.py syncdb --noinput >/dev/null 2>/dev/null
+count=0
+syncdbmax=10
+while [ $retcode -ne 0 -o $count -eq $syncdbmax ]; do
+    docker-compose run --rm web python manage.py syncdb --noinput >/dev/null 2>/dev/null
+    count=`expr $count + 1`
     retcode=$?
 done
+if [ $count -eq $syncdbmax ]; then
+    echo "syncdb failed:"
+    docker-compose run --rm web python manage.py syncdb --noinput
+fi
